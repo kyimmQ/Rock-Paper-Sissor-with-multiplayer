ad02f47180077f4c24d42bb45e6f9ccef7d2f452
CacLe <118438265+kyimmQ@users.noreply.github.com>
2023-05-19 20:46:37 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main(){
     std::cout << "----------------------------------------------------------------------" << std::endl;
     std::cout << "                      Welcome to Rock - Paper - Sissor                " << std::endl;
     std::cout << "----------------------------------------------------------------------" << std::endl;
-    std::cout << "This is a version of Rock - Paper - Sissor supporting multiplayer implemented in C++ with OOP" << std::endl;
+    std::cout << "This is a Rock - Paper - Sissor version supporting multiplayer implemented in C++ with OOP" << std::endl;
     std::cout << "1: Rock" << std::endl << "2: Paper" << std::endl << "3: Sissor" << std::endl;
     
     while(true){
